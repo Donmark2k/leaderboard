b005960de103a4a97aabf97df059ea4b2704f2c6
Donmark2k <chukwuemekavp@gmail.com>
2023-03-04 05:29:24 +0100

    Add the error message

--- src/index.js
+++ src/index.js
@@ -8,15 +8,37 @@ const inputForm = document.getElementById('score-form');
 const refreshButton = document.getElementById('refresh');
 const userName = document.getElementById('name');
 const userScore = document.getElementById('score');
+const error = document.getElementById('error-message');
+error.style.visibility = 'hidden';
 
 refreshButton.addEventListener('click', () => {
   getScore();
 });
 
+
 inputForm.addEventListener('submit', (event) => {
   event.preventDefault();
+  const  nameRegex = /^[A-Za-z ]*$/;
+  const scoreRegex = /^[0-9]*$/;
+  
   const name = userName.value;
   const score = userScore.value;
-  postScore(name, score);
-  inputForm.reset();
+  let validateScore =scoreRegex.test(score);
+  let validateName = nameRegex.test(name);
+  error.style.visibility = 'visible';
+
+  if (name !== '' && score !== '') {
+    if (validateName && validateScore) {
+      postData(name, score);
+      inputForm.reset();
+    } else {
+      error.innerHTML = 'Please your name should contain only alphabet and your score only numbers'; 
+       }
+  } else {
+    error.innerHTML = 'Please enter your name and score'; 
+    }
+
+
+  // postScore(name, score);
+  // inputForm.reset();
 });
