f5c52502060a83b45267b5427929252cd93e4ab2
Donmark2k <chukwuemekavp@gmail.com>
2023-03-04 10:48:13 +0100

    Make URL dynamic for API endpoint

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,2 +1,2 @@
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/TtGg5D4J46kysqEHITKB/scores';
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/O59Cq1DSmp19KcYuz3WS/scores';
 export default url;
